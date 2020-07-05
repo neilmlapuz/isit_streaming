af1063bc2c2b9c56aa28a4ed5b059a4303a60f5e
neilmlapuz <neilmlapuz@gmail.com>
2020-07-05 23:10:33 +0100

    changed driver naming and added multiple region search

--- scripts/lookup.py
+++ scripts/lookup.py
@@ -32,20 +32,7 @@ class Lookup(object):
         return driver
 
 
-    def request_site(self):
-        chrome_browser = self.configure_driver()
-        search_q = self.movie_name
-        region = 'ie'
-        chrome_browser.get(f'https://www.justwatch.com/{region}/search?providers=nfx&q={search_q}')
-
-        try:
-            WebDriverWait(chrome_browser, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, 'navbar__search')))
-        except TimeoutException as e:
-            print('Wait Timed out')
-            print(e)
-
-        xml_page_result = BeautifulSoup(chrome_browser.page_source, "lxml")
-
+    def extract_movies_available(self, xml_page_result):
         for movie_title in xml_page_result.select('div.title-list-row'):
             for title_row in movie_title.select('.title-list-row__row'):
                 title_selector = 'span.title-list-row__row__title'
@@ -57,6 +44,31 @@ class Lookup(object):
                     'year':title_row.select_one(year_selector).text,
                     'seasons':title_row.select_one(season_selector).text 
                 })
+        print('---------------------------------')
 
-        chrome_browser.quit()
+    def request_site(self):
+        search_q = self.movie_name
+        regions = ['ie', 'us', 'au']
+        ff_browser = self.configure_driver()
+
+
+        for reg in regions:
+            print(f'Region: {reg}')
+
+            ff_browser.get(f'https://www.justwatch.com/{reg}/search?providers=nfx&q={search_q}')
+
+            try:
+                WebDriverWait(ff_browser, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, 'title-list-row')))
+            except TimeoutException as e:
+                print('Wait Timed out')
+                print(e)
+
+            xml_page_result = BeautifulSoup(ff_browser.page_source, "lxml")
+
+            self.extract_movies_available(xml_page_result)
+
+        ff_browser.quit()
+
+
+
 
