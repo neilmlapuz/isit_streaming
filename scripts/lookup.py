8e545c0672ed2876d89a98c2ab47bd871f68e9db
neilmlapuz <neilmlapuz@gmail.com>
2020-07-05 17:59:00 +0100

    changed chrome to firefox driver and implemented movie extractors

--- scripts/lookup.py
+++ scripts/lookup.py
@@ -4,9 +4,11 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+
 
 class Lookup(object):
     movie_name = ''
@@ -19,34 +21,42 @@ class Lookup(object):
         self.request_site()
 
 
+    def configure_driver(self):
+        '''
+        configure webdriver to chrome and make it headless
+        '''
+        driver_options = Options()
+        driver_options.add_argument('--headless')
+        driver = webdriver.Firefox(executable_path='./geckodriver', firefox_options=driver_options)
+
+        return driver
+
+
     def request_site(self):
+        chrome_browser = self.configure_driver()
         search_q = self.movie_name
         region = 'ie'
-        chrome_browser = webdriver.Chrome('/Users/adminy/Downloads/chromedriver')
-        chrome_browser.get('https://www.justwatch.com/'+ region +'/search?providers=nfx&q='+search_q)
+        chrome_browser.get(f'https://www.justwatch.com/{region}/search?providers=nfx&q={search_q}')
 
         try:
-            WebDriverWait(chrome_browser, 30).until(
-                EC.element_to_be_clickable((By.CLASS_NAME, 'navbar__search')))
+            WebDriverWait(chrome_browser, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, 'navbar__search')))
         except TimeoutException as e:
             print('Wait Timed out')
             print(e)
 
-        # movies = chrome_browser.find_element_by_class_name('title-list-row')
+        xml_page_result = BeautifulSoup(chrome_browser.page_source, "lxml")
 
-        print(chrome_browser.page_source)
+        for movie_title in xml_page_result.select('div.title-list-row'):
+            for title_row in movie_title.select('.title-list-row__row'):
+                title_selector = 'span.title-list-row__row__title'
+                year_selector = 'span.title-list-row__row--muted'
+                season_selector = 'div.price-comparison__grid__row__price'
+                
+                print({
+                    'title':title_row.select_one(title_selector).text,
+                    'year':title_row.select_one(year_selector).text,
+                    'seasons':title_row.select_one(season_selector).text 
+                })
 
-        movies = [movie.text for movie in chrome_browser.find_elements_by_class_name('title-list-row__row')]
-        print(movies)
-
-        time.sleep(30)
-
-
-
-
-
-
-
-
-
+        chrome_browser.quit()
 
