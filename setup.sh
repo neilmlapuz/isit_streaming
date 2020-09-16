a30890076f9fd95afb2d9d2dc96524ba54d14dc8
neilmlapuz <neilmlapuz@gmail.com>
2020-09-16 11:59:01 +0100

    updated setup script

--- setup.sh
+++ setup.sh
@@ -3,11 +3,15 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     echo 'MAC'
     wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-macos.tar.gz
+    Package_name='geckodriver-v0.26.0-macos.tar.gz'
 elif [[ "$OSTYPE" == "linux"* ]]; then
     echo 'LINUX'
     wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
+    Package_name='geckodriver-v0.26.0-linux64.tar.gz'
+
 fi
 
-tar -xzvf geckodriver* && rm -rf geckodriver-v* && mv ./geckodriver* scripts
+tar -xzvf $Package_name
+rm -rf geckodriver-v* && mv ./geckodriver* scripts
 
 
